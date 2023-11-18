0cbb9a81bbb18979a756a1a8f9ce37e4bd25a521
em4go <emagodev@gmail.com>
2023-11-18 15:46:32 +0100

    First commit

---
+++ src/consts.ts
@@ -0,0 +1,42 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Ernesto Martínez - Data Scientist';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const projects = [
+	{
+		name: 'Movie recommendation web',
+		link: 'https://github.com/em4go/filmanin_proy',
+		img: '/portfolio/filmanin.png',
+		color: '#C33764',
+		stack: ['python', 'scikitlearn', 'fastapi', 'htmx', 'docker'],
+	},
+	{
+		name: 'Workout timer app',
+		link: 'https://github.com/em4go/calitimer',
+		img: '/portfolio/calitimer.png',
+		color: '#1DC984',
+		stack: ['javascript', 'svelte', 'pouchdb', 'css', 'html'],
+	},
+	{
+		name: 'Project 3',
+		link: 'https://github.com',
+		img: '/portfolio/filmanin.png',
+	},
+	{
+		name: 'Project 3',
+		link: 'https://github.com',
+		img: '/portfolio/filmanin.png',
+	},
+	{
+		name: 'Project 3',
+		link: 'https://github.com',
+		img: '/portfolio/filmanin.png',
+	},
+	{
+		name: 'Project 3',
+		link: 'https://github.com',
+		img: '/portfolio/filmanin.png',
+	},
+];
