901813fb590f1f9f1e2b6d3389256af0e240fefe
em4go <emagodev@gmail.com>
2023-11-20 22:22:30 +0100

    Re-design to spa + blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://em4go.github.io',
-	base: 'portfolio',
-	integrations: [mdx(), sitemap()],
+  site: 'https://em4go.github.io',
+  base: 'portfolio',
+  integrations: [mdx(), sitemap(), svelte()]
 });
