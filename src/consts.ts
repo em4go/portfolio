ae8ede74f63cef3dea11380c83969ce95cf3dd54
em4go <emagodev@gmail.com>
2024-01-10 18:34:18 +0100

    add seo optimization

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ernesto Martínez - Data Scientist';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-
 export const projects = [
 	{
 		name: 'Movie recommendation web',
