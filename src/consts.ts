aa0bb71fa74bcba87cebe13c861e647b6bfc8090
em4go <emagodev@gmail.com>
2024-01-10 17:14:18 +0100

    remove /portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -8,37 +8,37 @@ export const projects = [
 	{
 		name: 'Movie recommendation web',
 		link: 'https://github.com/em4go/filmanin_proy',
-		img: '/portfolio/filmanin.png',
+		img: 'filmanin.png',
 		color: '#C33764',
 		stack: ['python', 'scikitlearn', 'fastapi', 'htmx', 'docker'],
 	},
 	{
 		name: 'Under-18 athletics ranking analysis',
 		link: 'https://github.com/em4go/analisis-ranking-atletismo-2023',
-		img: '/portfolio/rank_u18_analysis.png',
+		img: 'rank_u18_analysis.png',
 		color: '#F7DF1E',
 		stack: ['python', 'pandas', 'streamlit', 'plotly'],
 	},
 	{
 		name: 'Workout timer app',
 		link: 'https://github.com/em4go/calitimer',
-		img: '/portfolio/calitimer.png',
+		img: 'calitimer.png',
 		color: '#1DC984',
 		stack: ['javascript', 'svelte', 'pouchdb', 'css', 'html'],
 	},
 	// {
 	// 	name: 'Project 3',
 	// 	link: 'https://github.com',
-	// 	img: '/portfolio/filmanin.png',
+	// 	img: 'filmanin.png',
 	// },
 	// {
 	// 	name: 'Project 3',
 	// 	link: 'https://github.com',
-	// 	img: '/portfolio/filmanin.png',
+	// 	img: 'filmanin.png',
 	// },
 	// {
 	// 	name: 'Project 3',
 	// 	link: 'https://github.com',
-	// 	img: '/portfolio/filmanin.png',
+	// 	img: 'filmanin.png',
 	// },
 ];
